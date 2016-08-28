b5569acb1fb83a6ef2ab9a2b6d81ca11c8e3bb25
peak <peakmuma@163.com>
2016-08-28 15:05:09 +0800

    'final类型变量初始化'

---
+++ me/peak/FinalTest.java
@@ -0,0 +1,20 @@
+package me.peak;
+
+public class FinalTest {
+	/*
+	 * 关于final类型变量的初始化
+	 * 非static类型：可以再声明中或在构造函数中初始化，只能初始化一次
+	 *  static类型：在声明中或者在static块中初始化
+	 */
+	public final FinalTest finaltest=null;
+	
+	public FinalTest(){
+		
+	}
+
+	public static void main(String[] args){
+		
+		FinalTest finaltest1;
+		finaltest1 = new FinalTest();
+	}
+}
