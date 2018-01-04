8b244b8401b6bf50aa32802425d7332740be0d03
peak <peakmuma@163.com>
2018-01-04 10:44:57 +0800

    tcp拆包处理

--- me/peak/netty/NIOSocketHandler.java
+++ me/peak/netty/NIOSocketHandler.java
@@ -11,28 +11,38 @@ public class NIOSocketHandler implements Runnable{
 
 	Logger logger = LoggerFactory.getLogger(NIOSocketHandler.class);
 
+	StringBuilder messageSB;
+
 	SocketChannel socketChannel;
+
 	NIOSocketHandler(SocketChannel channel){
 		this.socketChannel = channel;
+		messageSB = new StringBuilder();
 	}
+
 	@Override
 	public synchronized void run() {
 		ByteBuffer buffer = ByteBuffer.allocate(128);
 		try {
 			int res;
-			StringBuilder sb = new StringBuilder();
 			buffer.clear();
 			logger.info("---------start get message");
 			while ( (res = socketChannel.read(buffer)) > 0 ) {
 				logger.info("---------get message length " + res);
 				buffer.flip();
 				while (buffer.position() < buffer.limit()) {//todo < or <= ???
-					sb.append((char)buffer.get());
+					char c = (char)buffer.get();
+					if (c != (byte)4) {
+						messageSB.append(c);
+					} else {
+						processMessage(messageSB.toString());
+						messageSB.setLength(0);
+					}
 				}
-				processMessage(sb.toString());
 				buffer.clear();
 			}
 			if (res == -1) {
+				logger.info("---------client close socket");
 				socketChannel.close();
 			}
 			logger.info("---------get message over");
@@ -42,6 +52,6 @@ public class NIOSocketHandler implements Runnable{
 	}
 
 	public void processMessage(String message){
-		System.out.println(message);
+	    logger.info(message);
 	}
 }
